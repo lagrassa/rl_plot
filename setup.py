ea0b3478ea6a1dc6f154c62d69f6fc4d2409266e
Alex LaGrassa <lagrassa@mit.edu>
2019-01-22 12:08:55 -0500

    setup.py and restructure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='rl_plot',
+   version='1.0',
+   description='RL plotting utilities',
+   author='Alex LaGrassa',
+   author_email='lagrassa@mit.edu',
+   packages=['foo'],  #same as name
+   install_requires=['matplotlib', 'numpy'], #external packages as dependencies
+   )
